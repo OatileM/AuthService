e606ba09c7ed4c35e9f56ad722fa4ecdca3bfdac
Oatile Mdlela <osmdlela@gmail.com>
2025-02-17 22:59:28 +0200

    AWS RDS has been configured successfully with PostgreSQL

---
+++ Data/MyDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Data;
+using AuthService.Models;
+
+namespace AuthService.Data
+{
+    public class MyDbContext: DbContext
+    {
+
+        public MyDbContext(DbContextOptions<MyDbContext> options)
+       : base(options)
+        {
+        }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Role> Roles { get; set; }
+    }
+}
